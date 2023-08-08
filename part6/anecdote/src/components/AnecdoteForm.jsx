de41f80ce2eccf2b507ae95ff05c3a110a19eb70
Mustafasohail7 <mustufasohail7@gmail.com>
2023-08-08 18:21:04 +0500

    6.15

--- part6/anecdote/src/components/AnecdoteForm.jsx
+++ part6/anecdote/src/components/AnecdoteForm.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from "react-redux"
 import { addNote } from "../reducers/anecdoteReducer"
 import { changeText, removeText } from "../reducers/notificationReducer"
+import anecdoteService from "../services/anecdoteService"
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch()
 
-  const handleAdd = (event) => {
+  const handleAdd = async (event) => {
     event.preventDefault()
     const content = event.target.content.value
     event.target.content.value = ''
-    dispatch(addNote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(addNote(newAnecdote))
     dispatch(changeText(`you added '${content}'`))
     setTimeout(() => {
       dispatch(removeText())
