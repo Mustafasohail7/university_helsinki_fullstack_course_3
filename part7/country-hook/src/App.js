d41b8c38c49a4ce4b0419c2d4f122dfd060a5d05
Mustafasohail7 <mustufasohail7@gmail.com>
2023-08-09 15:01:18 +0500

    7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,20 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`).then(response => {
+      console.log(response.data)
+      const data = {
+        name: response.data.name.common,
+        capital: response.data.capital[0],
+        population: response.data.population,
+        flag: response.data.flags.png
+      }
+      setCountry({ found: true, data})
+    }).catch(error => {
+      setCountry({ found: false})
+    })
+  },[name])
 
   return country
 }
